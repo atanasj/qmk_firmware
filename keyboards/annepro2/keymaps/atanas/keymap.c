3d4f9529e302fd9f60312bcb4cc1ebcb088daa48
atanas <atanas.atanas@gmail.com>
2021-01-28 22:43:23 +1100

    change back _FN2 layer name for bluetooth

--- keyboards/annepro2/keymaps/atanas/keymap.c
+++ keyboards/annepro2/keymaps/atanas/keymap.c
@@ -4,12 +4,12 @@
 
 
 enum anne_pro_layers {
-  _BL, /* base layer */
-  _VI, /* vi navigation layer */
-  _NL, /* numpad layer */
-  _MS, /* mouse layer */
-  _ML, /* media layer */
-  _FN, /* function layer */
+  _BL,        /* base layer */
+  _VI,        /* vi navigation layer */
+  _NL,        /* numpad layer */
+  _MS,        /* mouse layer */
+  _FN2_LAYER, /* media and defualt FN2 layer */
+  _FN,        /* function layer */
 };
 
 /* macros */
@@ -57,20 +57,20 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 */
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BL] = KEYMAP( /* Base */
-    LT(_FN,KC_GRV), KC_1,         KC_2,    KC_3,            KC_4,            KC_5,           KC_6,          KC_7,           KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,        KC_BSPC,
-    LT(_MS,KC_TAB), KC_Q,         KC_W,    KC_E,            KC_R,            KC_T,           KC_Y,          KC_U,           KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,       RCMD_T(KC_BSLS),
-    LCTL_T(KC_ESC), KC_A,         KC_S,    LT(_VI,KC_D),    KC_F,            KC_G,           KC_H,          KC_J,           KC_K,    KC_L,   KC_SCLN, KC_QUOT, RCTL_T(KC_ENT),
-    KC_LSPO,        LT(_NL,KC_Z), KC_X,    KC_C,            KC_V,            KC_B,           KC_N,          KC_M,           KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-    HYPR_T(KC_F20), KC_LCMD,      KC_LALT, KC_SPC,          LCAG_T(KC_LEFT), MEH_T(KC_DOWN), LT(_FN,KC_UP), LT(_ML,KC_RIGHT)
+    LT(_FN,KC_GRV), KC_1,         KC_2,    KC_3,            KC_4,                KC_5,           KC_6,          KC_7,           KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,        KC_BSPC,
+    LCMD_T(KC_TAB), KC_Q,         KC_W,    LT(_MS,KC_E),    LT(_FN2_LAYER,KC_R), KC_T,           KC_Y,          KC_U,           KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,       RCMD_T(KC_BSLS),
+    LCTL_T(KC_ESC), KC_A,         KC_S,    LT(_VI,KC_D),    KC_F,                KC_G,           KC_H,          KC_J,           KC_K,    KC_L,   KC_SCLN, KC_QUOT, RCTL_T(KC_ENT),
+    KC_LSPO,        LT(_NL,KC_Z), KC_X,    KC_C,            KC_V,                KC_B,           KC_N,          KC_M,           KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+    HYPR_T(KC_F20), KC_LCMD,      KC_LALT, KC_SPC,          LCAG_T(KC_LEFT),     MEH_T(KC_DOWN), LT(_FN,KC_UP), LT(_FN2_LAYER,KC_RIGHT)
 ),
   /*
   * Layer _VI
   * ,-----------------------------------------------------------------------------------------.
   * |     |     |     |     |     |     |     |     |     |     |    |    |    |              |
   * |-----------------------------------------------------------------------------------------+
-  * |        |     |     |     |     |    |  HOME  | PGDN |  PGUP  |  END  |    |    |    |   \    |
+  * |        |     |     |     | _FN2_LAYER m|    |  HOME  | PGDN |  PGUP  |  END  |    |    |    |   \    |
   * |-----------------------------------------------------------------------------------------+
-  * | Esc     |KC_LALT |KC_LSFT |    |  KC_LCMD  |    |  LEFT  |  DOWN  |  UP  |  RIGHT  | | |    Enter    |
+  * | Esc  |ALT |SFT |   | CMD |   | LEFT | DOWN | UP | RIGHT |    |     |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * |            |    |   |   |    |    |    |  BSPC  |  DEL  |   |    |           |
   * |-----------------------------------------------------------------------------------------+
@@ -79,11 +79,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   *
   */
  [_VI] = KEYMAP( /* Base */
-    _______, KC_1,    KC_2,    KC_3,    KC_4,         KC_5,    KC_6,       KC_7,     KC_8,    KC_9,      KC_0,    KC_MINS,       KC_EQL,        KC_BSPC,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, MO(_ML),      MO(_FN), KC_HOME,    KC_PGDN,  KC_PGUP, KC_END,    XXXXXXX, SCMD(KC_LBRC), SCMD(KC_RBRC), XXXXXXX,
-    _______, KC_LALT, KC_LSFT, _______, KC_LCMD,      XXXXXXX, KC_LEFT,    KC_DOWN,  KC_UP,   KC_RIGHT,  KC_F19,  XXXXXXX,       _______,
-    XXXXXXX, MO(_NL), XXXXXXX, XXXXXXX, XXXXXXX,      M_DBW,   A(KC_BSPC), KC_BSPC,  KC_DEL,  A(KC_DEL), M_DFW,   XXXXXXX,
-    _______, _______, _______, _______, LCA(KC_LCMD), MOD_MEH, MO(_FN),    MO(_ML)
+    _______, KC_1,    KC_2,    KC_3,    KC_4,           KC_5,    KC_6,       KC_7,     KC_8,    KC_9,      KC_0,    KC_MINS,       KC_EQL,        KC_BSPC,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, MO(_FN2_LAYER), MO(_FN), KC_HOME,    KC_PGDN,  KC_PGUP, KC_END,    XXXXXXX, SCMD(KC_LBRC), SCMD(KC_RBRC), XXXXXXX,
+    _______, KC_LALT, KC_LSFT, _______, KC_LCMD,        XXXXXXX, KC_LEFT,    KC_DOWN,  KC_UP,   KC_RIGHT,  KC_F19,  XXXXXXX,       _______,
+    XXXXXXX, MO(_NL), XXXXXXX, XXXXXXX, XXXXXXX,        M_DBW,   A(KC_BSPC), KC_BSPC,  KC_DEL,  A(KC_DEL), M_DFW,   XXXXXXX,
+    _______, _______, _______, _______, LCA(KC_LCMD),   MOD_MEH, MO(_FN),    MO(_FN2_LAYER)
 ),
   /*
   * Layer _NL
@@ -123,14 +123,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   *
   */
  [_MS] = KEYMAP( /* Base */
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, KC_ACL2, KC_ACL0, KC_ACL1, MO(_ML),    XXXXXXX, KC_WH_L, KC_WH_U,    KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_MS_L, KC_MS_D,    KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, C(G(KC_D)), XXXXXXX, KC_BTN2, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_ACL0, KC_ACL2, KC_ACL1, _______, MO(_FN2_LAYER), XXXXXXX, KC_WH_L, KC_WH_U,    KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, KC_MS_L, KC_MS_D,    KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, C(G(KC_D)), XXXXXXX, KC_BTN2, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX
  ),
   /*
-  * Layer _ML
+  * Layer _FN2_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
   * |-----------------------------------------------------------------------------------------+
@@ -144,7 +144,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   * \-----------------------------------------------------------------------------------------/
   *
   */
- [_ML] = KEYMAP( /* Base */
+ [_FN2_LAYER] = KEYMAP( /* Base */
     KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, XXXXXXX, XXXXXXX, XXXXXXX, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, XXXXXXX, KC_AP2_BT_UNPAIR,
     RESET,      XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX,      KC_MPLY,                  KC_HOME,         KC_END,  XXXXXXX,
     KC_CAPS,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU,       KC_MNXT,      KC_PGUP,                  KC_PGDN,         XXXXXXX,
@@ -167,11 +167,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   *
   */
  [_FN] = KEYMAP( /* Base */
-    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MO(_ML), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_HOME, KC_END, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_DEL,  XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, MO(_ML)
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,          KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MO(_FN2_LAYER), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_HOME, KC_END, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_DEL,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, _______, MO(_FN2_LAYER)
 ),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
